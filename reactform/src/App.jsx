a93b10a37abb8494508e15f2ee04457c29d5c7aa
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-05-16 01:12:30 +0530

    code optimized

--- reactform/src/App.jsx
+++ reactform/src/App.jsx
@@ -1,40 +1,46 @@
-import "./App.css"
+import "./App.css";
 import { useState } from "react";
 import Card from "./components/card";
 import WarningLine from "./components/warning";
 import Password from "./components/password";
 function App() {
-
-  const [counter, setCounter] = useState(1)
-  const [warningCount, setWarningCount] = useState(1)
+  const [counter, setCounter] = useState(1);
+  const [warning, setWarning] = useState(false)
   const AddValue = () => {
-    warningCount < 21 ? setWarningCount(warningCount + 1) : setWarningCount(warningCount)
-    counter < 20 ?
-      setCounter(counter + 1) : setCounter(counter)
-
-  }
+    if (counter >= 20) {
+      setWarning(true)
+      return;
+    }
+    setWarning(false)
+    setCounter(counter + 1);
+  };
   const RemoveValue = () => {
-    warningCount > -1 ? setWarningCount(warningCount - 1) : setWarningCount(warningCount)
 
-    counter > 0 ? setCounter(counter - 1) : setCounter(counter)
-  }
+    if (counter <= 0) {
+      setWarning(true)
+      return;
+    }
+    setCounter(counter-1);
+    setWarning(false)
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Basic Counter</h1>
         <h2> Counter : {counter}</h2>
-        {console.log(warningCount)}{warningCount < 0 ? <WarningLine counter={warningCount}></WarningLine> : ""}
-        {warningCount > 20 ? <WarningLine counter={warningCount}></WarningLine> : ""}
 
+        <WarningLine counter={counter} warning={warning}></WarningLine>
         <div className="container">
-
-          <Card button="Boost me" onClick={AddValue} title="Add Card" ></Card>
-          <Card button="Reduce me" onClick={RemoveValue} title="Deduct Card" ></Card>
-          <Password ></Password>
-
+          <Card button="Boost me" onClick={AddValue} title="Add Card"></Card>
+          <Card
+            button="Reduce me"
+            onClick={RemoveValue}
+            title="Deduct Card"
+          ></Card>
+          <Password></Password>
         </div>
-      </header >
+      </header>
     </div>
   );
 }
--- reactform/src/components/warning.jsx
+++ reactform/src/components/warning.jsx
@@ -1,14 +1,14 @@
-const WarningLine = ({ counter }) => (
+const WarningLine = ({ counter, warning }) => (
     <div style={{ backgroundColor: '#F2DEDE', color: '#A94442', padding: '10px', borderRadius: '5px', border: '1px solid #A94442', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)' }}>
-        {counter < 0 ? (
+        {warning && counter <= 0 ? (
             <>
                 <span style={{ fontFamily: 'cursive', fontSize: '1.2rem', fontWeight: 'bold' }}>⚠ Warning:</span> You cannot go below zero.
             </>
-        ) : (
+        ) :warning && counter >= 20 ?  (
             <>
                 <span style={{ fontFamily: 'cursive', fontSize: '1.2rem', fontWeight: 'bold' }}>⚠ Warning:</span> You cannot go above 20.
             </>
-        )}
+        ) : <></>}
     </div>
 );
 
